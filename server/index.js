26e1b8634adafd44acbf4bdfc3faefdba6108d2c
mikhail guilbo <mikhailguilbo@MacBook-Pro-mikhail.local>
2025-07-16 11:44:25 +0200

    details finish4

--- server/index.js
+++ server/index.js
@@ -1,24 +1,33 @@
+require('dotenv').config()
+
+
 const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-require('dotenv').config()
+
 
 
 
 const app = express()
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || '*'
+}
+
+
+));
+
 app.use(bodyParser.json());
 
-app.post('/send',( req, res) => { //путь по которому передаються данные 
+app.get('/send', async ( req, res) => { //путь по которому передаються данные 
 const {name, email , message, phone} = req.body;//вытаскиваем все что нам надо из тела запроса
 
 
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
+  port: parseInt(process.env.SMTP_PORT,10),
   secure: process.env.EMAIL_SECURE === 'true',
    auth:{
 user: process.env.EMAIL_USER,
@@ -39,7 +48,7 @@ const mailOptions = {
   };
 
   try {
-   transporter.sendMail(mailOptions);
+   await transporter.sendMail(mailOptions);
     res.status(200).json({ success: true, message: 'Message sent successfully' });
   } catch (error) {
     console.error('Error sending email:', error);
@@ -48,7 +57,7 @@ const mailOptions = {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () =>{
 
    console.log(`сервер запушен на порту ${PORT}`) 
